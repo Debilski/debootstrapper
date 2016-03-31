c5289dce6fd8ee58197b6517e10b02be8506ca8c
Rike-Benjamin Schuppner <rikebs@debilski.de>
2016-03-31 16:59:53 +0200

    ENH: Automatically choose vg name.

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 set -euo pipefail
 
 source config
+HOSTNAME=$(basename $(hostname -A) ${DOMAIN})
+
+echo "Assuming hostname is:"
+echo $HOSTNAME
+
+VG=$HOSTNAME-vg
+echo "This means, we’ll create a volume group with the name ‘$VG’."
+echo "Please exit, if this is wrong."
+
+echo ""
 
 echo "Choose partition to install to:"
 read PARTITION
@@ -17,20 +27,20 @@ sgdisk --new=1:0:+512M --typecode=1:EF00 --change-name=1:"EFI Boot" $PARTITION
 sgdisk --new=2:0:+256M --typecode=2:8300 --change-name=2:"Boot" $PARTITION
 sgdisk --new=3:0:0     --typecode=3:8E00 --change-name=3:"LVM" $PARTITION
 
-vgcreate vg-main ${PARTITION}3
-lvcreate vg-main --size +70G --name root
-lvcreate vg-main --size +12G --name swap
-lvcreate vg-main --extents 100%FREE --name extra
+vgcreate $VG ${PARTITION}3
+lvcreate $VG --size +70G --name root
+lvcreate $VG --size +12G --name swap
+lvcreate $VG --extents 100%FREE --name extra
 
 mkfs.vfat -F 32 ${PARTITION}1
 mkfs.ext2 ${PARTITION}2
 
-mkfs.xfs /dev/vg-main/root
-mkswap /dev/vg-main/swap
-mkfs.xfs /dev/vg-main/extra
+mkfs.xfs /dev/$VG/root
+mkswap /dev/$VG/swap
+mkfs.xfs /dev/$VG/extra
 
 mkdir -p /target
-mount /dev/vg-main/root /target
+mount /dev/$VG/root /target
 mkdir -p /target/boot
 mount ${PARTITION}2 /target/boot
 mkdir -p /target/boot/efi
@@ -59,12 +69,12 @@ sync
 sleep 3
 
 umount -A --recursive /target/
-mount /dev/vg-main/root /target
+mount /dev/$VG/root /target
 mount ${PARTITION}2 /target/boot
 mount ${PARTITION}1 /target/boot/efi
 
 cat > /target/root/default-environment <<EOF
-HOSTNAME=$(basename $(hostname -A) ${DOMAIN})
+HOSTNAME=${HOSTNAME}
 TIMEZONE=${TIMEZONE}
 EOF
 
