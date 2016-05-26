a91ba1784524511f5044e53ac8bc1c0dc1814dea
Rike-Benjamin Schuppner <rikebs@debilski.de>
2016-05-26 18:08:01 +0200

    NF: Adding the USB stick installer.

---
+++ install-stick.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+set -euo pipefail
+
+source config
+
+read -s -p "Please set the password for root: " PASSWD
+
+HOSTNAME=debian-boot # $(basename $(hostname -A) ${DOMAIN})
+
+echo "Assuming hostname is:"
+echo $HOSTNAME
+
+VG=debian-boot-vg
+echo "This means, we’ll create a volume group with the name ‘$VG’."
+echo "Please exit, if this is wrong."
+
+echo ""
+
+echo "Choose disk to install to:"
+read DISK
+
+echo "Current partition layout on ${DISK} is:"
+sgdisk -p $DISK
+
+echo "Deleting all data. Press button or quit with CTRL-C"
+read
+
+sgdisk --clear --zap-all --mbrtogpt $DISK
+sgdisk --new=1:0:+512M --typecode=1:EF00 --change-name=1:"EFI Boot" $DISK
+sgdisk --new=2:0:+256M --typecode=2:8300 --change-name=2:"Boot" $DISK
+sgdisk --new=3:0:0     --typecode=3:8E00 --change-name=3:"LVM" $DISK
+
+partprobe $DISK
+
+LVM_PARTITION=$(findfs PARTUUID=$(partx -o UUID -g -r --nr 3 $DISK))
+BOOT_PARTITION=$(findfs PARTUUID=$(partx -o UUID -g -r --nr 2 $DISK))
+EFI_PARTITION=$(findfs PARTUUID=$(partx -o UUID -g -r --nr 1 $DISK))
+
+echo "Please confirm the automatic selection of partitions:"
+echo "${EFI_PARTITION} for EFI"
+echo "${BOOT_PARTITION} for Boot"
+echo "${LVM_PARTITION} for LVM."
+read
+
+vgcreate $VG ${LVM_PARTITION}
+lvcreate $VG --size +3G --name root
+lvcreate $VG --extents 100%FREE --name extra
+
+mkfs.vfat -F 32 ${EFI_PARTITION}
+mkfs.ext2 ${BOOT_PARTITION}
+
+mkfs.xfs /dev/$VG/root
+mkfs.xfs /dev/$VG/extra
+
+echo "Installing to tmpfs."
+mkdir -p /target-tmpfs
+mount -t tmpfs -o size=500M none /target-tmpfs
+debootstrap --arch amd64 jessie /target-tmpfs http://${APT_CACHE}ftp.de.debian.org/debian
+
+mkdir -p /target
+mount /dev/$VG/root /target
+mkdir -p /target/boot
+mount ${BOOT_PARTITION} /target/boot
+mkdir -p /target/boot/efi
+mount ${EFI_PARTITION} /target/boot/efi
+mkdir -p /target/extra
+
+echo "Copying over to target."
+cp -a --preserve=all /target-tmpfs/* /target/
+sync
+umount /target-tmpfs
+
+cat >>/target/etc/locale.gen <<EOF
+en_US.UTF-8 UTF-8
+EOF
+
+cat >/target/etc/default/locale <<EOF
+LANG="en_US.UTF-8"
+LANGUAGE="en_US:en"
+EOF
+
+cat > /target/root/default-environment <<EOF
+HOSTNAME=${HOSTNAME}
+TIMEZONE=${TIMEZONE}
+EOF
+
+cp minimal-dhcp-network /target/etc/network/interfaces
+
+SYSTEMD_START_FILE=/target/etc/systemd/system/multi-user.target.wants/init-system.service
+cat >$SYSTEMD_START_FILE <<EOF
+[Service]
+Type=oneshot
+EnvironmentFile=/root/default-environment
+ExecStart=/usr/bin/hostnamectl set-hostname $HOSTNAME
+ExecStart=/usr/bin/timedatectl set-timezone $TIMEZONE
+ExecStart=/bin/systemctl poweroff
+EOF
+
+
+mount -t tmpfs -o size=500M none /target/var/cache
+mount -t tmpfs -o size=500M none /target/var/tmp
+
+systemd-nspawn -D /target apt-get install -y dbus openssh-server aptitude bash-completion locales
+# systemd-nspawn -D /target bash -c 'apt-get install -y $(tasksel --task-packages standard)'
+systemd-nspawn -D /target -b
+rm $SYSTEMD_START_FILE
+
+sed -i -e s/main/"main contrib non-free"/g /target/etc/apt/sources.list
+bash mkfstab-stick.sh $DISK $VG > /target/etc/fstab
+
+CHROOT_MOUNTS="dev dev/pts proc sys sys/firmware"
+for m in $CHROOT_MOUNTS ; do
+  mount --bind /$m /target/$m
+done
+
+chroot /target update-locale
+echo "root:${PASSWD}" | chroot /target chpasswd
+chroot /target apt-get update
+chroot /target apt-get install -y lvm2 xfsprogs linux-image-amd64 grub-efi-amd64 firmware-linux
+
+chroot /target grub-install --force-extra-removable --recheck $DISK
+chroot /target update-grub
+
+echo "Now umounting the dev mounts again. But sleeping a bit before that."
+sync
+sleep 3
+
+umount -A --recursive /target/
+
---
+++ mkfstab-stick.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+DISK=$1
+VG_NAME=$2
+
+BOOT_PARTITION=$(findfs PARTUUID=$(partx -o UUID -g -r --nr 2 $DISK))
+EFI_PARTITION=$(findfs PARTUUID=$(partx -o UUID -g -r --nr 1 $DISK))
+
+root=$(dmsetup info -C --noheadings -o Name /dev/$VG_NAME/root)
+extra=$(dmsetup info -C --noheadings -o Name /dev/$VG_NAME/extra)
+
+cat <<EOF
+# /etc/fstab: static file system information.
+# Use 'blkid' to print the universally unique identifier for a
+# device; this may be used with UUID= as a more robust way to name devices
+# that works even if disks are added and removed. See fstab(5).
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+UUID=$( blkid -s UUID -o value ${EFI_PARTITION} ) /boot/efi vfat umask=0077 0 1
+UUID=$( blkid -s UUID -o value ${BOOT_PARTITION} ) /boot ext2 defaults 0 2
+/dev/mapper/$root / xfs defaults 0 2
+/dev/mapper/$extra /extra xfs defaults 0 1
+EOF
+
